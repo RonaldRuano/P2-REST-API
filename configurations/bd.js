757c33cd1b67826f8d483e9b7a2efbda0c694214
unknown <rruanov1@miumg.edu.gt>
2021-09-14 17:05:53 -0600

    Archivos

---
+++ configurations/bd.js
@@ -0,0 +1,9 @@
+const host ="cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+const user = "ky3m7xbjlrshq5ey";
+const Password = "grpz7t9giuc02t2k";
+const database = "e5dfatkn8i4hwljf";
+
+exports.host=host;
+exports.user=user;
+exports.Password=Password;
+exports.database=database;
---
+++ index.js
@@ -0,0 +1,261 @@
+index.js
+const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+const bdConfig = require('./configurations/bd');
+
+var app = express();
+app.use(bodyParser.json());
+
+var mysqlConnection = mysql.createConnection({
+    host: bdConfig.host,
+    user: bdConfig.user,
+    password: bdConfig.Password,
+
+
+});
+
+//Crear Persona
+app.post("/Crearpersonas", (req, res) => {
+    console.log("crear Personas ");
+    let est = req.body;
+    console.log(est);
+    mysqlConnection.query('insert into persona (nombre, apellido, fecha_nacimiento, Direccion) values (?,?,?,?)',
+        [est.Nombre, est.Apellido, est.fecha_nacimiento, est.Direccion], (err, result) => {
+            if (!err) {
+                console.log(result);
+                
+                res.status(201).send("created");
+            } else {
+                console.log(err);
+                res.send('error' + err);
+            }
+        });
+});
+
+//Leer tododas las personas
+app.get("/Leerpersonas", (req, res) => {
+    console.log("get lista Personas");
+    mysqlConnection.query('Select * from persona', (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log(err);
+            res.send('error' + err);
+        }
+    });
+});
+
+//Buscar una persona
+app.get("/Leerunapersona/:id", (req, res) => {
+    console.log("get Persona");
+    mysqlConnection.query('Select * from persona where id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log(err);
+            res.send('error' + err);
+        }
+    });
+});
+
+//Actualizar persona
+app.put("/Actualizarpersona/:id", (req, res) => {
+    console.log("update persona ");
+    let est = req.body;
+    console.log(est);
+    mysqlConnection.query('update persona set nombre = ?, apellido = ?, fecha_nacimiento = ?, Direccion = ? where id = ?',
+        [est.Nombre, est.Apellido, est.fecha_nacimiento, est.Direccion, req.params.id], (err, result) => {
+            if (!err) {
+                console.log(result);
+                
+                res.status(202).send("updated");
+            } else {
+                console.log(err);
+                res.send('error' + err);
+            }
+        });
+});
+
+//Eliminar Persona
+app.delete("/Eliminarpersona/:id", (req, res) => {
+    console.log("update persona ");
+    mysqlConnection.query('delete from persona where id = ?',
+        [ req.params.id], (err, result) => {
+            if (!err) {
+                console.log(result);
+                
+                res.status(202).send("deleted");
+            } else {
+                console.log(err);
+                res.send('error' + err);
+            }
+        });
+});
+
+//Crear Maestro
+app.post("/Crearmaestro", (req, res) => {
+    console.log("crear Maestro ");
+    let est = req.body;
+    console.log(est);
+    mysqlConnection.query('insert into docente (id_persona, fecha_ingreso) values (?,?)',
+        [est.id_persona, est.fecha_ingreso], (err, result) => {
+            if (!err) {
+                console.log(result);
+                
+                res.status(201).send("created");
+            } else {
+                console.log(err);
+                res.send('error' + err);
+            }
+        });
+});
+
+//Leer todos los maestro
+app.get("/Leermaestro", (req, res) => {
+    console.log("get lista maestro");
+    mysqlConnection.query('Select * from docente', (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log(err);
+            res.send('error' + err);
+        }
+    });
+});
+
+//Buscar a un maestro
+app.get("/Leerunmaestro/:id", (req, res) => {
+    console.log("get maestro");
+    mysqlConnection.query('Select * from docente where id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log(err);
+            res.send('error' + err);
+        }
+    });
+});
+
+//Actualizar a un maestro
+app.put("/Actualizarmaestro/:id", (req, res) => {
+    console.log("update maestro ");
+    let est = req.body;
+    console.log(est);
+    mysqlConnection.query('update docente set id_persona = ?, fecha_ingreso = ? where id = ?',
+        [est.id_persona, est.fecha_ingreso, req.params.id], (err, result) => {
+            if (!err) {
+                console.log(result);
+                
+                res.status(202).send("updated");
+            } else {
+                console.log(err);
+                res.send('error' + err);
+            }
+        });
+});
+
+//Eliminar maestro
+app.delete("/Eliminarmaestro/:id", (req, res) => {
+    console.log("update maestro ");
+    mysqlConnection.query('delete from docente where id = ?',
+        [ req.params.id], (err, result) => {
+            if (!err) {
+                console.log(result);
+                
+                res.status(202).send("deleted");
+            } else {
+                console.log(err);
+                res.send('error' + err);
+            }
+        });
+});
+
+//Crear Estudiante
+app.post("/Crearestudiante", (req, res) => {
+    console.log("crear estudiante ");
+    let est = req.body;
+    console.log(est);
+    mysqlConnection.query('insert into estudiante (id_persona, fecha_ingreso, carnet, status) values (?,?,?,?)',
+        [est.id_persona, est.fecha_ingreso, est.carnet, est.status], (err, result) => {
+            if (!err) {
+                console.log(result);
+                
+                res.status(201).send("created");
+            } else {
+                console.log(err);
+                res.send('error' + err);
+            }
+        });
+});
+
+//Leer todos los estudiantes
+app.get("/Leerestudiante", (req, res) => {
+    console.log("get lista estudiantes");
+    mysqlConnection.query('Select * from estudiante', (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log(err);
+            res.send('error' + err);
+        }
+    });
+});
+
+//Buscar a un estudiante
+app.get("/Leerunestudiante/:id", (req, res) => {
+    console.log("get estudiante");
+    mysqlConnection.query('Select * from estudiante where id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log(err);
+            res.send('error' + err);
+        }
+    });
+});
+
+//Actualizar Estudiante
+app.put("/Actualizarestudiante/:id", (req, res) => {
+    console.log("update estudiante ");
+    let est = req.body;
+    console.log(est);
+    mysqlConnection.query('update estudiante set id_persona = ?, fecha_ingreso = ?, carnet = ?, statu = ? where id = ?',
+        [est.id_persona, est.fecha_ingreso, est.carnet, est.status, req.params.id], (err, result) => {
+            if (!err) {
+                console.log(result);
+                
+                res.status(202).send("updated");
+            } else {
+                console.log(err);
+                res.send('error' + err);
+            }
+        });
+});
+
+//Eliminar Estudiante
+app.delete("/Eliminarestudiante/:id", (req, res) => {
+    console.log("update estudiante ");
+    mysqlConnection.query('delete from estudiante where id = ?',
+        [ req.params.id], (err, result) => {
+            if (!err) {
+                console.log(result);
+                
+                res.status(202).send("deleted");
+            } else {
+                console.log(err);
+                res.send('error' + err);
+            }
+        });
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT ||3000);
